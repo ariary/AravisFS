6196268eee1f903f1ea068be5a327af198650418
kali <ariary9.2@hotmail.fr>
2021-07-29 17:12:57 -0400

    Parse a request for LS ok

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -50,9 +50,28 @@ func GetRequestLsJSON(path string, resourceName string) string {
 	return string(requestLs)
 }
 
+// Use to get the ls parameter of a request (as string format) ie  get body of a RequestLs or BodyLs struct
+// Behind the scene it transforms the request in requestLs struct. Then retrieve the body
+// (by Unmarshall it into a BodyLs struct)
+func GetBodyLsFromRequest(request string) BodyLs {
+	//We now that the request is (or should be) a RequestLs so transform it
+	requestLs := RequestLs{}
+	json.Unmarshal([]byte(request), &requestLs)
+
+	bodyJson, err := json.Marshal(requestLs.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	bodyLs := BodyLs{}
+	json.Unmarshal(bodyJson, &bodyLs)
+
+	return bodyLs
+}
+
 type Request struct {
 	Name string `json:"name"`
-	Body string `json:"type"`
+	Body []byte `json:"body"`
 }
 
 // Put a listener for adret call. It parse the request and call the appropriate function consequently
@@ -104,8 +123,8 @@ func echo(conn net.Conn) {
 }
 
 func main() {
-	fmt.Println(GetRequestLsJSON("encyrpted.arafs", "toto/tata"))
-	//equal {"name":"ls","body":{"path":"encyrpted.arafs","name":"toto/tata"}}
+	fmt.Println(GetRequestLsJSON("encrypted.arafs", "toto/tata"))
+	//equal {"name":"ls","body":{"path":"encrypted.arafs","name":"toto/tata"}}
 
 	//adret side
 	request := GetRequestLsJSON("encyrpted.arafs", "toto/tata")
@@ -115,17 +134,14 @@ func main() {
 	json.Unmarshal([]byte(request), &requestOnUbacSide)
 	fmt.Println(requestOnUbacSide.Name)
 	if requestOnUbacSide.Name == LS {
-		//this is a ls request => build requestLS struct to parse it
-		requestLs, err := json.Marshal(requestOnUbacSide.Body)
-		if err != nil {
-			panic(err)
-		}
-		bodyLs := BodyLs{}
-		json.Unmarshal(requestLs, &bodyLs)
+		//this is a ls request => build requestLs struct to parse it and get parameter
+		bodyLs := GetBodyLsFromRequest(request)
 
+		path := bodyLs.Path
+		resourceName := bodyLs.ResourceName
 		//now call Printls
-		fmt.Println("(on ubac side) path: ", bodyLs.Path)
-		fmt.Println("(on ubac side) resource name: ", bodyLs.ResourceName)
+		fmt.Println("(on ubac side) path: ", path)
+		fmt.Println("(on ubac side) resource name: ", resourceName)
 	}
 
 	//UbacListen(20080)
